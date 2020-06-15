3ce34cf24cfe1cac03def03e757ef69ecccd8abe
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-06-15 11:42:24 -0500

    Update __init__.py
    Added the new MainHandler

--- bftool/__init__.py
+++ bftool/__init__.py
@@ -1,9 +1,11 @@
-from .processhandler import ProcessHandler
-from .threadhandler import ThreadHandler
+from .ProcessHandler import ProcessHandler
+from .ThreadHandler import ThreadHandler
 from .useful_functions import wordlist_divider
 from .useful_functions import merge_wordlists
 from .useful_functions import import_module_from_path
 from .useful_functions import get_wordlists
+from .useful_functions import get_arguments
+from .MainHandler import MainHandler
 
 
 class Modes(object):
