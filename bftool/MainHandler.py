c17fcdf0f4fbf75ffb4e878f94e89a68b87b0a8e
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-06-17 14:10:17 -0500

    Update MainHandler.py
    - Now it shows the running time
    - Now it supports the Arguments class

--- bftool/MainHandler.py
+++ bftool/MainHandler.py
@@ -1,7 +1,9 @@
 import multiprocessing
+import time
 import threading
 import sys
 import bftool
+import bftool.ArgumentConstructor
 import bftool.WordlistHandler
 import bftool.ProcessHandler
 
@@ -25,36 +27,30 @@ class MainHandler(object):
         while not self.__print_queue.empty():
             print(self.__print_queue.get())
 
-    def main(self):
-        arguments = bftool.get_arguments()
+    def main(self, arguments: bftool.ArgumentConstructor.Arguments = None):
+        total_time = time.time()
+        if arguments is None:
+            arguments = bftool.get_arguments()
 
         # Get the function to be used by the handlers
-        module = bftool.import_module_from_path(arguments.module_path)
-        function_ = getattr(module, arguments.function_name)
+        function_ = arguments.function
 
-        wordlist_handler = bftool.WordlistHandler.WordlistHandler()
         wordlist_queue = multiprocessing.Queue()
 
-        numeric_mode = bftool.Modes.WORDLIST_BLOCK if arguments.mode == "wordlist" else bftool.Modes.BASIC_MODE
-
-        if arguments.wordlist:
-            wordlist_handler.wordlist_from_files(*arguments.wordlist)
-        if arguments.bruteforce:
-            wordlist_handler.pure_bruteforce_setup(*arguments.bruteforce)
-        wordlist_handler.setup()
-
         # Start filling the queue
-        wordlist_queue_thread = threading.Thread(target=bftool.arguments_queue_handler, args=(wordlist_queue,
-                                                                                              wordlist_handler))
+        wordlist_queue_thread = threading.Thread(target=bftool.arguments_queue_handler,
+                                                 args=(wordlist_queue, arguments.wordlist_handler)
+                                                 )
         wordlist_queue_thread.start()
 
         self.__print_queue.put("--- Starting child processes ---")
         print_queue_thread = threading.Thread(target=self.print_queue_handler, )
         print_queue_thread.start()
-        for index in range(1, arguments.max_processes+1):
+        processes_setup_time = time.time()
+        for index in range(1, arguments.maximum_number_of_concurrent_processes+1):
             process_handler = bftool.ProcessHandler.ProcessHandler(index, function_, wordlist_queue,
-                                                                   arguments.max_threads,
-                                                                   numeric_mode,
+                                                                   arguments.maximum_number_of_process_threads,
+                                                                   arguments.fuzzing_mode,
                                                                    self.__print_queue)
             if sys.platform == "win32":
                 self.start_win32_process(process_handler)
@@ -62,14 +58,21 @@ class MainHandler(object):
                 self.__processes.append(process_handler)
 
             self.__print_queue.put(f"* Process with ID {index} - Prepared")
+        processes_setup_time = time.time() - processes_setup_time
         self.__print_queue.put("--- All processes were prepared ---")
         self.__print_queue.put("--- Waiting to finish ---")
+        fuzzing_time = time.time()
         for process in self.__processes:
             process.start()
         for process_handler in self.__processes:
             process_handler.join()
-        self.__finish = True
+        fuzzing_time = time.time() - fuzzing_time
         wordlist_queue_thread.join()
+        total_time = time.time() - total_time
         self.__print_queue.put("--- END ---")
+        self.__print_queue.put(f"Time setting up the processes: {processes_setup_time}")
+        self.__print_queue.put(f"Time fuzzing: {fuzzing_time}")
+        self.__print_queue.put(f"Total time: {total_time}")
+        self.__finish = True
         print_queue_thread.join()
         exit(0)
