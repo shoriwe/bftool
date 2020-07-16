26be938fb2c72bd2d1f196861dcb9650d1a5a378
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2020-07-16 14:46:18 -0500

    Update Types.py

--- bftool/Types.py
+++ bftool/Types.py
@@ -1,5 +1,4 @@
-import typing
-import types
+import collections
 
 
 # Low cost way to backup python generators for re-iterations
@@ -7,7 +6,7 @@ class SpecialGenerator(object):
     """Low cost way to backup python generators for re-iterations, so always use it when you whant to pass generators to
     `bftool.Arguments` so the arguments distribution generator (the master one) can understand how to revert it to it's
     original state"""
-    def __init__(self, generator_function: types.FunctionType, *args):
+    def __init__(self, generator_function: collections.abc.Callable, *args):
         """
         Arguments:
             - generator_function: The function that creates the generator
@@ -18,25 +17,6 @@ class SpecialGenerator(object):
 
     def __call__(self, *args, **kwargs):
         return self.function(*self.arguments)
-
-
-# Only a Fancy way to specify how those wordlist need to be prepared
-class IterableWordlists(typing.Dict):
-    """Structure of a Wordlist of iterables"""
-    argument: int or str
-    wordlist_iterable: typing.Iterable
-
-
-class BruteforceWordlists(typing.Dict):
-    """Structure of a wordlist of bruteforce rules"""
-    argument: int or str
-    rule: SpecialGenerator
-
-
-class FilesWordlists(typing.Dict):
-    """Structure of a wordlist of files"""
-    argument: int or str
-    file_path: SpecialGenerator
 
 
 # Easies way to not expand arguments that are tuple
